a91f73fa1718fd375f0bbcf8dad29c126bfa521a
Phoonum Saelao <phoonumsaelao@Phoonums-MacBook-Pro.local>
2024-07-07 08:55:17 +0700

    first commit

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from random import randint
+
+app = FastAPI()
+
+@app.get('/')
+def HelloWorld():
+    return {"Hello": "World"}
+
+@app.get('/coinflip')
+def coinflip():
+    return {"result": "head" if randint(0,1) == 1 else "tail"}
+
+@app.get('/random')
+def randomNumber():
+    return {"result":randint(1,100)}
